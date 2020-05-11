fefb9c2c5b07c2ddac84ed71128bf4449e2b4364
devashish-acharya <thedev96@gmail.com>
2020-05-11 15:27:59 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function clickChanged() {
-  document.getElementById("thisOne").innerHTML = "Here is some music";
+  document.getElementById("thisOne").innerHTML = "Free Bird - Lynrd Skynyrd, Stairway to Heaven - Led zeppelin, Comfortably Numb - Pink Floyd";
+ }
+
+ function clickChangedTwo() {
+  document.getElementById("secondOne").innerHTML = "You shook me all night long - ACDC, Livin' on a prayer - Bon Jovi, Every rose has it's thorns - Poison";
  }
